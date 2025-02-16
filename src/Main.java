329b0ca85b783d40a47626b153194e5a5d408bd8
Slava ASUS <popovbs@gmail.com>
2025-02-16 13:02:37 +0300

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // Задача №1
+        System.out.println("Задача №1:");
+        int total = 0;
+        int salary = 15000;
+        int i;
+        for (i = 1; total <= 2_459_000; i++) {
+            total = total + total / 100;
+            total = total + salary;
+            System.out.println("Месяц " + i + " сумма накоплений равна " + total + " рублей");
+        }
+        System.out.println("Потребовалось " + i + " месяцев для накопления: " + total + " рублей");
+        System.out.println();
+
     }
 }
